9288e2551d209d08c72ea6a4df12da2ed5cc4c24
Leonardo Bilhalva Silva <bilhalvaleonardo1@gmail.com>
2024-08-08 00:45:01 -0300

    Exercicios em python

--- inicio.py
+++ inicio.py
@@ -16,44 +16,83 @@ import math
 # segundos = segundos%60
 # horas = minutos//60
 # minutos = minutos%60
-
 # print("Horas: ", horas, 
 #       "\nMinutos: ", minutos, 
 #       "\nSegundos: ", segundos)
-
-# #Inverter número
+##Inverter número
 # valor = int(input("Digite um valor de 4 digitos: "))
-
 # digito1 = valor//1000
 # resto = valor%1000
 # digito2 = resto//100
 # resto = resto%100
 # digito3 = resto//10
 # digito4 = resto%10
-
 # print(digito4*1000+digito3*100+digito2*10+digito1)
 
-# #Calculo peso
+##Calculo peso
 # altura = float(input("Digite a altura: "))
 # sexo = str(input("Digite o sexo (Masculino = M; Feminino = F): "))
-
 # if(sexo == "F" or sexo == "f"):
 #     print("Peso ideal feminino: ", 62.1*altura - 44.7)
 # elif(sexo == "M" or sexo == "m"):
 #     print("Peso ideal masculino: ", 72.7*altura - 58)
 # else: print("Sexo inválido")
 
-
-# #Horário do jogo
+##Horário do jogo
 # hora = int(input("Digite o valor da HORA que INICIOU o jogo: "))
 # minuto = int(input("Digite o valor da MINUTO que INICIOU o jogo: "))
 # inicio = hora*60+minuto
 # hora = int(input("Digite o valor da HORA que FINALIZOU o jogo: "))
 # minuto = int(input("Digite o valor da MINUTO que FINALIZOU o jogo: "))
 # fim = hora*60+minuto
-
 # if(inicio >= fim):
 #     duração = fim+60*24 - inicio
 # else: duração = fim - inicio
-
 # print("A duração do jogo foi de",duração//60,"horas e",duração%60,"minutos")
+
+##Número Capicua
+# numero = int(input("Digite 4 digitos inteiros: "))
+
+# if(numero < 1111 and numero > 9999): print("Numero inválido!")
+# else:
+#     numeroInv = numero//1000 + ((numero%1000)//100)*10 + ((numero%100)//10)*100 + (numero%10)*1000
+#     if(numero == numeroInv): print("Números capicua!:",numero,numeroInv)
+#     else: print("Não são números capcua!",numero,numeroInv)
+
+##Ordem Crescente 3 valores
+# valor1 = int(input("Digite o primeiro valor: "))
+# valor2 = int(input("Digite o segundo valor: "))
+# if(valor2 < valor1):
+#     cache = valor1
+#     valor1 = valor2
+#     valor2 = cache
+# valor3 = int(input("Digite o terceiro valor: "))
+# if(valor3 < valor1):
+#     cache = valor1
+#     valor1 = valor3
+#     valor3 = valor2
+#     valor2 = cache
+# if(valor3 < valor2):
+#     cache = valor2
+#     valor2 = valor3
+#     valor3 = cache
+# print(valor1, valor2, valor3)
+
+##Ordem Crescente e decrescente 3 valores
+# valor1 = int(input("Digite o primeiro valor: "))
+# valor2 = int(input("Digite o segundo valor: "))
+# valor3 = int(input("Digite o terceiro valor: "))
+
+# maior = valor1
+# if(valor2 > maior): maior = valor2
+# if(valor3 > maior): maior = valor3
+
+# menor = valor1
+# if(valor2 < menor): maior = valor2
+# if(valor3 < menor): maior = valor3
+
+# meio = valor1+valor2+valor3 - maior - menor
+# print("Crescente:",menor,meio,maior)
+# print("Decrescente:",maior,meio,menor)
+
+
